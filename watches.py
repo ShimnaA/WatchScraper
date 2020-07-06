2e78dbb922dd2412c57411bb1808bfbc8899ea70
shimna <shimnaa.arangil@gmail.com>
2020-07-06 15:47:35 +0530

    Collect all brands, and details of each watch

--- watches.py
+++ watches.py
@@ -16,26 +16,59 @@ class WatchScraper:
         self.base_url = "https://www.ethoswatches.com/brands.html"
         self.driver = webdriver.Chrome(ChromeDriverManager().install())
         self.driver.implicitly_wait(50)  # Set impicit waits for 50 seconds
+        self.brand_list = []
+
 
     def get_brands(self):
         soup = BeautifulSoup(self.driver.page_source, 'html.parser')
         brands = soup.find(class_='items am_shopby_filter_items_attr_manufacturer')
         brands = brands.find_all(class_='item')
-        brand_list = []
+
         for brand in brands:
             link = brand.find('a').get('href')
             label = brand.get_text()
             label_count_list = label[3:].split('\n')
             label = label_count_list[0]
             watchcount = int(label_count_list[1])
-            brand_list.append(Brand(label, link, watchcount))
-        print(brand_list)
+            self.brand_list.append(Brand(label, link, watchcount))
+        print(self.brand_list)
 
+    def get_productlist(self,brandlink):
+        self.driver.get(brandlink)
+        soup = BeautifulSoup(self.driver.page_source, 'html.parser')
+        watchlist1 = soup.find(class_='columns')
+        watchlist_soup = watchlist1.div.find_all('li')
+        product_list = []
+        for watch in watchlist_soup:
+            if watch.find('h2') is not None:
+                link = watch.find('a').get('href')
+                product_list.append(link)
+        return product_list
+
+    def get_watchdetails(self,product_list):
+        for product_link in product_list:
+            print(product_link)
+            self.driver.get(product_link)
+            soup = BeautifulSoup(self.driver.page_source, 'html.parser')
+            product_info = soup.find(class_='product_info_main')
+
+            print(product_info.h1.span.text.strip())
+            print(product_info.h3.text.strip())
+            print(product_info.find(class_='price').text)
+            print(product_info.find(class_='product_emi').text.strip())
+            image_info = soup.find(class_='openPhotoSwipe').findChildren("img")
+            image_info_tag = image_info[0]["alt"]
+            image_info_src = image_info[0]["src"]
+            print(image_info_tag)
+            print(image_info_src)
 
     def main_logic(self):
         self.driver.get(self.base_url)
         self.driver.maximize_window()
         self.get_brands()
+        for brand in self.brand_list:
+            product_list = self.get_productlist(brand.link)
+            self.get_watchdetails(product_list)
 
 
 
